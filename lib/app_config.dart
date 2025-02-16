1f62d81d3ee3509ce4f499d60020b5060e0957f0
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-16 17:08:43 +0330

    Add AppConfig class with project and database identifiers

---
+++ lib/app_config.dart
@@ -0,0 +1,5 @@
+class AppConfig {
+  static const String projectId = "67b1b35500384d26d8d6";
+  static const String databaseId = "67b1bfaf0014ad8a783c";
+  static const String databaseCollectionId = "67b1bfcc0032b6aace17";
+}
